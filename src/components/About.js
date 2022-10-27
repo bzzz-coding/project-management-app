13a0562af1880aaaa92a4d527b74f09c658eb490
bzzz-coding <86077274+bzzz-coding@users.noreply.github.com>
2022-10-27 14:49:20 -0700

    Styled about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,12 @@
 import { Link } from 'react-router-dom'
+import Button from "./Button";
 
 const About = () => {
   return (
-    <div>
-        <h4>Version 0.001</h4>
-        <Link to="/">Go Back</Link>
+    <div className='text-center'>
+      
+      <h4 className='text-yellow-500'>Version 0.001</h4>
+      <Link className='mx-auto' to='/'><Button text='Go Back'/></Link>
     </div>
   )
 }
