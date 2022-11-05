7688461e48c37ea0f7defac87a62d861d0e2baf3
bzzz-coding <86077274+bzzz-coding@users.noreply.github.com>
2022-11-04 20:42:53 -0700

    Styled AddTask

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -29,44 +29,41 @@ const AddTask = ({ onAdd }) => {
   }
 
   return (
-    <form className="add-form" onSubmit={onSubmit}>
-      <div className="form-control">
-        <label>Task</label>
-        <input type='text' placeholder='Add Project' value={text} onChange={(e) => setText(e.target.value)} />
-      </div>
-      <div className="form-control">
-        <label>Day & Time</label>
-        {/* <input type='text' placeholder='Add Day and Time' value={day} onChange={(e) => setDay(e.target.value)} /> */}
-        <input type='date' min={moment().format('YYYY-MM-DD')} placeholder='Add Day and Time' value={day} onChange={(e) => setDay(e.target.value)}></input>
-      </div>
-      {/* priority */}
-      <div className="form-control">
-        <label>Priority</label>
-        <select className='select-menu w-full mt-1' name='priority' min='' value={priority} onChange={(e) => setPriority(e.target.value)}>
-          <option value='none'>Select Priority Level</option>
-          <option value='low'>Low</option>
-          <option value='medium'>Medium</option>
-          <option value='high'>High</option>
-        </select>
+    <form className="form-control md:w-1/2 xl:w-1/3 mx-auto" onSubmit={onSubmit}>
+      <label className="mt-3 label">
+        <span className="label-text">Project Title</span>
+      </label>
+      <input type='text' placeholder='Add Project' value={text} onChange={(e) => setText(e.target.value)} className="input input-bordered w-full" />
+
+      <label className="mt-3 label">
+        <span className="label-text">Due Date</span>
+      </label>
+      <input type='date' min={moment().format('YYYY-MM-DD')} placeholder='Add Day and Time' value={day} onChange={(e) => setDay(e.target.value)} className="input input-bordered w-full" />
+
+      <label className="mt-3 label">
+        <span className="label-text">Priority</span>
+      </label>
+      <select name='priority' value={priority} onChange={(e) => setPriority(e.target.value)} className="select select-bordered">
+        <option value='none'>Select Priority Level</option>
+        <option value='low'>Low</option>
+        <option value='medium'>Medium</option>
+        <option value='high'>High</option>
+      </select>
+
+      <label className="mt-3 label">
+        <span className="label-text">Progress %</span>
+      </label>
+      <input required type='number' min='0' max='100' placeholder='Progress' value={progress} onChange={(e) => setProgress(e.target.value)} className="input input-bordered w-full" />
+      <div className="progress-bar w-full mt-1 bg-gray-100 rounded-full h-2.5">
+        <div className="bg-neutral h-2.5 rounded-full" style={{ width: `${progress}%` }}></div>
       </div>
 
-      {/* progress */}
-      <div className="form-control">
-        <label>Progress</label>
-        <input required type='number' min='0' max='100' placeholder='Progress' value={progress} onChange={(e) => setProgress(e.target.value)} />
-        {/* <progress value={progress} max='100'></progress> */}
-        <div className="progress-bar w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
-          <div className="bg-purple-600 h-2.5 rounded-full dark:bg-purple-500" style={{width: `${progress}%`}}></div>
-        </div>
-      </div>
+      <label className="mt-3 label">
+        <span className="label-text">Note</span>
+      </label>
+      <textarea value={note} onChange={(e) => setNote(e.target.value)} className="textarea textarea-bordered h-24" placeholder="Note"></textarea>
 
-      <div className="form-control form-control-check">
-        <label>Set Reminder</label>
-        <input type='checkbox' checked={reminder} value={reminder} onChange={(e) => setReminder(e.currentTarget.checked)} />
-      </div>
-      <label>Note</label>
-      <textarea className='w-full mt-1' value={note} onChange={(e) => setNote(e.target.value)}></textarea>
-      <input className="btn btn-accent" type='submit' value='Save Task' />
+      <input className="btn btn-accent my-6 w-36" type='submit' value='Save Project' />
     </form>
   )
 }
